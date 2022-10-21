897559520ccddd36c1c941a522a6b51b3bebc93d
Cfmunoz8 <cfmunoz8@gmail.com>
2022-10-21 20:03:17 +0000

    creación de tablas

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-from models import db
+from models import db, User, Favorite, Character, Planet, Vehicle
 
 
 app = Flask(__name__)
-
 # Sqlite
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///starwars.db"
-
 db.init_app(app)
+
+#with app.app_context():
+#    db.create_all()
+
+
+
+
+app.run(host="localhost", port=8080)
