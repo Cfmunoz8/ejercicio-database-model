9ac4d42ca44a209579aee0bd80b7da8f29cbc49b
Cfmunoz8 <cfmunoz8@gmail.com>
2022-10-23 16:56:52 +0000

    funciones select and delete

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///starwars.db"
 db.init_app(app)
     
 #funciones para agregar una fila
+
 def add_user():
     user = User()
-    user.name = "Camila"
+    user.name = "Francisca"
     user.email = "c@c.com"
     user.password = "c12345"
 
@@ -21,34 +22,34 @@ def add_user():
 def add_favorite():
     favorite = Favorite()
     favorite.name = "Luke Skywalker"
-    favorite.user_id = 1
+    favorite.user_id = 2
 
     db.session.add(favorite)
     db.session.commit()
 
 def add_character():
     character = Character()
-    character.name = "Luke Skywalker"
-    character.hair_color = "blond"
-    character.birth_year = "19BBY"
+    character.name = "Darth Vader"
+    character.hair_color = "unknow"
+    character.birth_year = "41.9BBY"
 
     db.session.add(character)
     db.session.commit()
 
 def add_planet():
     planet = Planet()
-    planet.name = "Tatooine"
-    planet.population = 200000
-    planet.character_id = 1
+    planet.name = "Alderaan"
+    planet.population = 200000000
+    planet.character_id = 2
 
     db.session.add(planet)
     db.session.commit()
 
 def add_vehicle():
     vehicle = Vehicle()
-    vehicle.name = "X wing"
-    vehicle.crew = 1
-    vehicle.character_id = 1
+    vehicle.name = "Death Star"
+    vehicle.crew = 1000000
+    vehicle.character_id = 2
 
     db.session.add(vehicle)
     db.session.commit()
@@ -87,10 +88,68 @@ def update_vehicle(id):
 
 #funciones select
 
+def select_user():
+    users = User.query.all()
+    users = list(map(lambda user: user.name, users))
 
+    print(users)
 
+def select_favorite():
+    favorites = Favorite.query.all()
+    favorites = list(map(lambda favorite: favorite.name, favorites))
+
+    print(favorites)
+
+def select_character():
+    characters = Character.query.all()
+    characters = list(map(lambda character: character.name, characters))
+
+    print(characters)
+
+def select_planet():
+    planets = Planet.query.all()
+    planets = list(map(lambda planet: planet.name, planets))
+
+    print(planets)
+
+def select_vehicle():
+    vehicles = Vehicle.query.all()
+    vehicles = list(map(lambda vehicle: vehicle.name, vehicles))
+
+    print(vehicles)
 
 #funciones delete
+
+def delete_favorite(id):
+    favorite = Favorite.query.get(id)
+
+    db.session.delete(favorite)
+    db.session.commit()
+
+def delete_user(id):
+    user = User.query.get(id)
+
+    db.session.delete(user)
+    db.session.commit()
+
+def delete_character(id):
+    character = Character.query.get(id)
+
+    db.session.delete(character)
+    db.session.commit()
+
+def delete_planet(id):
+    planet = Planet.query.get(id)
+
+    db.session.delete(planet)
+    db.session.commit()
+
+def delete_vehicle(id):
+    vehicle = Vehicle.query.get(id)
+
+    db.session.delete(vehicle)
+    db.session.commit()
+
 
 
 with app.app_context():
@@ -105,8 +164,16 @@ with app.app_context():
     #update_character(1)
     #update_planet(1)
     #update_vehicle(1)
-
-
+    #select_user()
+    #select_favorite()
+    #select_character()
+    #select_planet()
+    #select_vehicle()
+    #delete_favorite(2)
+    #delete_user(2)
+    delete_character(2)
+    delete_planet(2)
+    delete_vehicle(2)
 
 
 
